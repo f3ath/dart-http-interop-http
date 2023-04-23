2150774543755ae22cbec238b75b1cc96b4d6c52
Alexey <f3ath@users.noreply.github.com>
2023-04-22 22:47:29 -0400

    v0.1 (#1)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,5 +1,10 @@
+## [0.1.0] - 2023-04-22
+### Changed
+- Bump deps
+
 ## [0.0.1] - 2023-04-16
 ### Added
 - Initial commit
 
+[0.1.0]: https://github.com/f3ath/dart-http-interop-http/compare/0.0.1...0.1.0
 [0.0.1]: https://github.com/f3ath/dart-http-interop-http/releases/tag/0.0.1
--- example/example.dart
+++ example/example.dart
@@ -3,7 +3,7 @@ import 'package:http_interop_http/http_interop_http.dart';
 
 Future<void> main() async {
   final handler = DisposableHandler();
-  final request = Request('get', Uri.parse('https://example.com'));
+  final request = HttpRequest('get', Uri.parse('https://example.com'));
   final response = await handler.handle(request);
   print(response.statusCode);
   print(response.headers);
