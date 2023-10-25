1dac692e201351d7b7238e6f0b97cb8d7c3b5ef0
f3ath <karapetov@gmail.com>
2023-10-24 19:09:05 -0700

    0.7

--- lib/http_interop_http.dart
+++ lib/http_interop_http.dart
@@ -1,4 +1,4 @@
 export 'package:http_interop_http/src/client_wrapper.dart';
-export 'package:http_interop_http/src/disposable_handler.dart';
+export 'package:http_interop_http/src/one_off_handler.dart';
 export 'package:http_interop_http/src/header_folding.dart';
 export 'package:http_interop_http/src/standard_folding.dart';
